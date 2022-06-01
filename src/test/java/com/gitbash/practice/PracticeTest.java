6bc030d6980bf90e193467bdd3cf848ffe0df7f2
Dhanraj-DR1 <drd60303@gmail.com>
2022-06-01 14:42:16 +0530

    project1

---
+++ src/test/java/com/gitbash/practice/PracticeTest.java
@@ -0,0 +1,13 @@
+package com.gitbash.practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+	
+	@Test
+	public void practiceTest() {
+		Reporter.log("WELCOME",true);
+	}
+
+}
