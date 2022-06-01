a59ec695f6c3adc3982e028e54df46759d1fc192
Dhanraj-DR1 <drd60303@gmail.com>
2022-06-01 15:51:23 +0530

    msg

--- src/test/java/com/gitbash/practice/PracticeTest.java
+++ src/test/java/com/gitbash/practice/PracticeTest.java
@@ -4,7 +4,7 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class PracticeTest {
-	//changes from b2
+	//changes from b1
 	@Test
 	public void practiceTest() {
 		Reporter.log("WELCOME",true);
