0deddd26e50a8e524c8be6e019b669012cd51b1e
Dhanraj-DR1 <106527643+Dhanraj-DR1@users.noreply.github.com>
2022-06-05 19:11:03 +0530

    chnages made by dhanraj

--- src/test/java/com/gitbash/practice/PracticeTest.java
+++ src/test/java/com/gitbash/practice/PracticeTest.java
@@ -4,7 +4,7 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class PracticeTest {
-	//changes from b2
+	//changes from dhanraj
 	@Test
 	public void practiceTest() {
 		Reporter.log("WELCOME",true);
